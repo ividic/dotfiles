a6012532f030759093172b4684ee0833ef13e4a6
Igor V <igor@incrementalmarketing.com.au>
2015-10-14 16:42:23 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# DOTFILES
+
+## Introduction
+
+
+## Installation
+1. Fork this repository
+2. Clone to your machine
+3. Run install.sh
+    1. It will prompt you for the basic Git info, unless specified
+    2. Custom Git info should be saved in ~/.gitconfig.local
+4. Customise ~/bash.local as needed
+
+
+## Editing
+
+
+
+### TODO:
+1. OSX defaults
+2. Backup - see below
+
+
+### TODO: git
+
+1. pretty diff (@paulirish) + uncomment in gitconfig
+
+
+### TODO: shell
+
+
+### TODO: Aliases/Functions
+
+1. z - github.com/rupa/z
+2. Software updates ala https://github.com/alrra/dotfiles/blob/master/shell/bash_aliases or https://github.com/mathiasbynens/dotfiles/blob/master/.aliases
+
+
+### Syntax highlighting for these files
+
+If you edit this stuff, install [Dotfiles Syntax Highlighting](https://github.com/mattbanks/dotfiles-syntax-highlighting-st2) via [Package Control](http://wbond.net/sublime_packages/package_control)
+
+
+## Inspiration
+
+https://github.com/paulirish/dotfiles
+https://github.com/mathiasbynens/dotfiles/
+https://github.com/alrra/dotfiles/
+https://github.com/necolas/dotfiles
+https://github.com/holman/dotfiles
+
+
+## Interesting
+
+http://nicolasgallagher.com/mac-osx-bootable-backup-drive-with-rsync/
---
+++ install.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+# Install - Symlink config files
+# https://github.com/paulirish/dotfiles/blob/master/symlink-setup.sh
+# https://github.com/alrra/dotfiles/blob/master/os/create_symbolic_links.sh
+#
+# Bash conditional statements: http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_01.html
+#
+# 1. Include utils
+# 2.
+# 3. Set $PATH in bash_profile
+# 4. Set chmod +x in shell/bin
+#
+
+cd "$(dirname "$BASH_SOURCE")" \
+	&& source 'shell/utils/utils.sh'
+
+#declare dotfilesDirectory="$HOME/Work/projects/_dotfiles"
+declare dotfilesDirectory="$(pwd)"
+
+
+check_git_local() {
+
+    local gitconfig_local_file="$(pwd)/git/gitconfig.local"
+
+    if [ ! -e $gitconfig_local_file ]; then
+        # Prompt for name and email
+        print_error "$gitconfig_local_file not found!"
+        echo "Your name:"
+        read git_name
+        echo "Your email:"
+        read git_email
+
+        # Print to config file
+        echo "[user]" > $gitconfig_local_file
+        echo "  name = $git_name" >> $gitconfig_local_file
+        echo "  email = $git_email" >> $gitconfig_local_file
+    else
+        print_success "Using existing $gitconfig_local_file"
+    fi
+
+}
+
+check_git_local
+
+
+# finds all .dotfiles in this folder
+#declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
+#FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim bin" # add in vim and the binaries
+declare -a FILES_TO_SYMLINK=(
+
+    'shell/bash_aliases'
+    'shell/bash_exports'
+    'shell/bash_functions'
+    'shell/bash_logout'
+    'shell/bash_options'
+    'shell/bash_profile'
+    'shell/bash_prompt'
+    'shell/bash.local'
+
+    'shell/bashrc'
+    'shell/curlrc'
+    'shell/inputrc'
+    'shell/screenrc'
+
+    'git/gitattributes'
+    'git/gitconfig'
+    'git/gitconfig.local'
+    'git/gitignore_global'
+
+)
+
+main() {
+
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+    local backupDir="$HOME/.backup_originals"
+    local skipOriginalsCheck="$HOME/.skiporiginalscheck"
+
+
+    # Shall we back up the original files?
+    ask_for_confirmation "Backup current config files?"
+    if answer_is_yes; then
+
+        if [ -e $backupDir ]; then
+            ask_for_confirmation "Backup dir ($backupDir) already exists, overwrite files within?"
+            if answer_is_yes; then
+                print_info "Will overwrite existing files in backup directory"
+            else
+                print_error "Please remove/rename backup dir ($backupDir) and re-run the installation script"
+                exit;
+            fi
+        else
+            mkdir -p $backupDir
+        fi
+
+        for i in ${FILES_TO_SYMLINK[@]}; do
+
+            originalFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+            if [ -e "$originalFile" ]; then
+                execute "cp -af $originalFile $backupDir/" "$originalFile → $backupDir"
+            fi
+
+        done
+    else
+        if [ ! -e "$skipOriginalsCheck" ]; then
+            # Give the user one last chance to change their mind
+            print_question "Are you sure? All the original files will be overwritten."
+            print_info "Run the install script again if you are sure, this message won't appear again."
+            touch "$skipOriginalsCheck"
+            exit;
+        fi
+    fi
+
+
+    # Loop through the files and symlink
+    for i in ${FILES_TO_SYMLINK[@]}; do
+
+        sourceFile="$(pwd)/$i"
+        targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+        if [ -e "$sourceFile" ]; then
+            if [ -e "$targetFile" ]; then
+                if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+
+                    ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                    if answer_is_yes; then
+                        rm -rf "$targetFile"
+                        execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                    else
+                        print_error "$targetFile → $sourceFile"
+                    fi
+
+                else
+                    print_success "$targetFile → $sourceFile"
+                fi
+            else
+                execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+            fi
+        else
+            print_error "$sourceFile is missing!"
+        fi
+
+    done
+
+}
+
+main
