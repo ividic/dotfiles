e434b0cf633cc3c78641174007f2af2f137b992d
Igor Vidic <ividic@gmail.com>
2026-06-12 12:00:46 +1000

    Apps: Clean up

--- os/install-apps.sh
+++ os/install-apps.sh
@@ -4,7 +4,7 @@ source '../shell/utils/utils.sh'
 
 ask_for_confirmation "Install Homebrew?"
 if answer_is_yes; then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update brew
@@ -15,21 +15,14 @@ brew upgrade
 
 # Install apps using Cask
 brew install --cask anki
-brew install --cask brave-browser
-brew install --cask clipgrab
+brew install --cask altair-graphql-client
 brew install --cask docker
 brew install --cask dropbox
-brew install --cask firefox
 brew install --cask gas-mask
 brew install --cask ghostty
 brew install --cask google-chrome
 brew install --cask google-chrome-canary
-brew install --cask imagealpha
-brew install --cask imageoptim
-brew install --cask iterm2
 brew install --cask kap
-brew install --cask karabiner-elements
-brew install --cask licecap
 brew install --cask menumeters
 brew install --cask microsoft-edge
 brew install --cask microsoft-office
@@ -39,18 +32,18 @@ brew install --cask postman
 brew install --cask raycast
 brew install --cask royal-tsx
 brew install --cask sequel-ace
-brew install --cask skitch
 brew install --cask slack
-brew install --cask unnaturalscrollwheels
 brew install --cask virtualbox
 brew install --cask visual-studio-code
+brew install --cask vivaldi
 brew install --cask vlc
 
 # Improve QuickLook in Finder
-brew install --cask qlcolorcode qlstephen qlmarkdown quicklook-json
-brew install --cask quicklook-csv betterzip qlimagesize webpquicklook suspicious-package
+brew install --cask jpc-qlcolorcode qlstephen qlmarkdown quicklook-json
+brew install --cask quicklook-csv betterzip suspicious-package
 
 # Other useful apps
+brew install f2
 brew install rename
 brew install ssh-copy-id
 brew install bash-completion
